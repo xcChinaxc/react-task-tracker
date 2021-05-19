c9056970b27a8b738c9b06dd278cef7a48d922dc
xcChinaxc <67491204+xcChinaxc@users.noreply.github.com>
2021-05-19 00:43:39 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTask] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Fed 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Shopping',
+      day: 'Fed 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Jog',
+      day: 'Fed 5th at 2:30pm',
+      reminder: false,
+    },
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title='Task Tracker'/>
+      <Tasks tasks={tasks}  />
     </div>
   );
 }
