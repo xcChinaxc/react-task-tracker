efc8c0b103609c4918b28cf1b6e6c92e380beccf
xcChinaxc <67491204+xcChinaxc@users.noreply.github.com>
2021-05-20 22:26:51 -0400

    final commit

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,7 +4,7 @@ const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
         <>
             {tasks.map((task, index) => (
-                <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
+                <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
             ))}
         </>
     )
